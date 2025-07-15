7342c72dd5f8f7e0d4a8f70b2089af80c804bf18
pya-h <p4ya.py@gmail.com>
2025-07-16 02:24:30 +0330

    add inevntory for investments

--- anchor/programs/raffles/src/lib.rs
+++ anchor/programs/raffles/src/lib.rs
@@ -15,6 +15,8 @@ pub enum RafflesErrors {
 
 #[program]
 pub mod raffles {
+    use anchor_lang::solana_program::example_mocks::solana_sdk::system_program;
+
     use super::*;
 
     pub fn initialize_id_counter(ctx: Context<InitializeIdCounter>) -> Result<()> {
@@ -24,18 +26,58 @@ pub mod raffles {
         Ok(())
     }
 
-    pub fn initialize_vault(ctx: Context<InitializeVault>, capacity: u32, close_at: u64) -> Result<()> {
-        let vault = &mut ctx.accounts.vault;
+    pub fn initialize_vault(
+        ctx: Context<InitializeVault>,
+        capacity: u32,
+        close_at: u64,
+    ) -> Result<()> {
+        let vault: &mut Account<'_, Vault> = &mut ctx.accounts.vault_info;
         vault.id = ctx.accounts.current_id.value;
         vault.authority = *ctx.accounts.creator.key;
         ctx.accounts.current_id.value += 1;
         vault.participants = 0;
         vault.capacity = capacity;
-        vault.bump = ctx.bumps.vault;
+        vault.bump = ctx.bumps.vault_info;
         vault.close_at = close_at;
         vault.created_at = ctx.accounts.clock.unix_timestamp as u64;
-        require!(vault.close_at == 0 || vault.close_at > vault.created_at, RafflesErrors::CloseDateHasPassed);
+        require!(
+            vault.close_at == 0 || vault.close_at > vault.created_at,
+            RafflesErrors::CloseDateHasPassed
+        );
         vault.pool = 0;
+
+        let (vault_inventory_pda, inventory_bump) = Pubkey::find_program_address(
+            &[
+                b"vault",
+                ctx.accounts.creator.key.as_ref(),
+                ctx.accounts.current_id.value.to_le_bytes().as_ref(),
+            ],
+            &ctx.program_id,
+        );
+
+        let min_lamports = Rent::get()?.minimum_balance(0);
+        let cpi_accounts = anchor_lang::solana_program::system_instruction::create_account(
+            ctx.accounts.creator.key,
+            &vault_inventory_pda,
+            min_lamports,
+            0,
+            &system_program::ID,
+        );
+
+        anchor_lang::solana_program::program::invoke_signed(
+            &cpi_accounts,
+            &[
+                ctx.accounts.creator.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[&[
+                b"vault",
+                ctx.accounts.creator.key.as_ref(),
+                ctx.accounts.current_id.value.to_le_bytes().as_ref(),
+                &[inventory_bump],
+            ]],
+        )?;
+        vault.inventory = vault_inventory_pda;
         Ok(())
     }
 }
@@ -54,7 +96,7 @@ pub struct InitializeIdCounter<'info> {
     )]
     pub id_counter: Account<'info, IdCounter>,
 
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -71,12 +113,20 @@ pub struct InitializeVault<'info> {
     #[account(
         init,
         payer=creator,
-        seeds=[b"vault", creator.key.as_ref(), current_id.value.to_le_bytes().as_ref()],
+        seeds=[b"vault-info", creator.key.as_ref(), current_id.value.to_le_bytes().as_ref()],
         space=8 + Vault::INIT_SPACE,
         bump
     )]
-    pub vault: Account<'info, Vault>,
+    pub vault_info: Account<'info, Vault>,
 
+    // #[account(
+    //     init,
+    //     payer = creator,
+    //     space = 0,
+    //     seeds = [b"vault", creator.key.as_ref(), current_id.value.to_le_bytes().as_ref()],
+    //     bump
+    // )]
+    // pub vault: Account<'info, Vault>,
     pub system_program: Program<'info, System>,
 
     pub clock: Sysvar<'info, Clock>,
@@ -93,6 +143,7 @@ pub struct Vault {
     pub close_at: u64,
     pub created_at: u64,
     pub id: u64,
+    pub inventory: Pubkey,
 }
 
 #[account]
